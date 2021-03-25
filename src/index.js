9482fb50bd8f7f34cc644e35ce3da43f5fe055f3
ramesh.srinivasan <rsrinivasan@appointmentplus.com>
2021-03-25 16:28:22 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { firebase } from '@firebase/app';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBV0izfGMt2rouV_06sFae2XPPL21mHxbE",
+  authDomain: "savetext-in.firebaseapp.com",
+  projectId: "savetext-in",
+  storageBucket: "savetext-in.appspot.com",
+  messagingSenderId: "993332262987",
+  appId: "1:993332262987:web:d3efd919aaca9a3be9f5ce",
+  measurementId: "G-B1BH2YB895"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
